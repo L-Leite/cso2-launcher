261fc24a17203cbe1903a5d65d62939ffa3645ed
Ochii <ochii@outlook.pt>
2018-08-25 16:32:47 +0100

    Temporarily remove ent_create cheat flag
    Until sv_cheats gets fixed

---
+++ src/launcher/hooks/server.cpp
@@ -0,0 +1,17 @@
+#include "convar.h"
+#include "hooks.h"
+
+void BytePatchServer(uintptr_t dwServerBase)
+{
+	// remove FCVAR_CHEAT from ent_create until sv_cheats is fixed
+	ConCommand* ent_create = (ConCommand*)(dwServerBase + 0xA0875C);
+
+	if (ent_create->IsFlagSet(FCVAR_CHEAT))
+		ent_create->RemoveFlags(FCVAR_CHEAT);
+}
+
+ON_LOAD_LIB(server)
+{
+	uintptr_t dwServerBase = GET_LOAD_LIB_MODULE();
+	BytePatchServer(dwServerBase);
+}
