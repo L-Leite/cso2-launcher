fc89ec83508fc64668c895c3019d5143aefdaf47
Ochii <ochii@outlook.pt>
2018-09-13 00:20:19 +0100

    Fix module list

--- src/launcher/modulelist.h
+++ src/launcher/modulelist.h
@@ -1,21 +1,27 @@
 #pragma once
 
-#include <stdint.h>
+#include <cstdint>
 #include <map>
 
+// stores module base addresses
+// if there is no module entry, look it up with GetModuleHandle
+// it should be cheaper than calling GetModuleHandle multiple times
+// maybe replace std::map with a faster container
 class CModuleList
 {
 public:
-	CModuleList();
+	CModuleList() = default;
 	~CModuleList() = default;
+	CModuleList(const CModuleList&) = delete;
+	CModuleList& operator=(const CModuleList&) = delete;
 
-	uintptr_t Get(const wchar_t* szModuleName);
+	uintptr_t Get(const std::string& szModuleName);
 
 protected:
-	bool LookupModule(const wchar_t* szModuleName);
+	bool LookupModule(const std::string& szModuleName);
 
 private:
-	std::map<const wchar_t*, uintptr_t> m_Modules;
+	std::map<const std::string, uintptr_t> m_Modules;
 };
 
 extern CModuleList g_ModuleList;
