e8a5be92c011bda30b09a8841501d02e089093b5
Ochii <ochii@outlook.pt>
2018-09-14 11:49:18 +0100

    Once setuplibs.ps1 is finished, change the current directory to the project's root

--- setuplibs.ps1
+++ setuplibs.ps1
@@ -1,21 +1,24 @@
 # setup PolyHook 2
-Set-Location ./libs/PolyHook_2_0
+Set-Location .\libs\PolyHook_2_0
 
 # ugly hack, find a way to pass this through command line
-$lastLine = (Get-Content -tail 1 ./CMakeLists.txt)
+$lastLine = (Get-Content -tail 1 .\CMakeLists.txt)
 if ($lastLine -ne "add_definitions(-D_ITERATOR_DEBUG_LEVEL=0)")
 {
 	Add-Content .\CMakeLists.txt "`nadd_definitions(-D_ITERATOR_DEBUG_LEVEL=0)"
 }
 
-Set-Location ./capstone
+Set-Location .\capstone
 mkdir build32
 mkdir build64
-Set-Location ./build32
-cmake -G "Visual Studio 15 2017" ../
-Set-Location ../../
-cmake -G "Visual Studio 15 2017" -DBUILD_DLL=ON -DBUILD_STATIC=OFF ./
+Set-Location .\build32
+cmake -G "Visual Studio 15 2017" ..\
+Set-Location ..\..\
+cmake -G "Visual Studio 15 2017" -DBUILD_DLL=ON -DBUILD_STATIC=OFF .\
 
 # setup source sdk
 Set-Location ..\source-sdk-cso2
 .\createlauncher.bat
+
+# get back to the project's root
+Set-Location ..\..
