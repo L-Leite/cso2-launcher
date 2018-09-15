1919c33d4a7800cdb4cf4207e35a384456623628
Ochii <ochii@outlook.pt>
2018-09-15 02:03:07 +0100

    Fix relay byte patches' array sizes and disable BugTrap on engine.dll and hook Sys_SpewFunc again

--- src/launcher/hooks/engine.cpp
+++ src/launcher/hooks/engine.cpp
@@ -12,6 +12,14 @@ void ConnectEngineLibraries( const uintptr_t dwEngineBase )
 	}
 }
 
+HOOK_DETOUR_DECLARE( hkSys_SpewFunc );
+
+NOINLINE int hkSys_SpewFunc( int spewType, char* pMsg )
+{
+	std::cout << pMsg;
+	return HOOK_DETOUR_GET_ORIG( hkSys_SpewFunc )(spewType, pMsg);
+}
+
 struct IpAddressInfo
 {
 	std::string szIpAddress;
@@ -26,27 +34,14 @@ NOINLINE void __fastcall hkGetServerIpAddressInfo( IpAddressInfo* pIpAddressInfo
 	pIpAddressInfo->uPort = 30001;
 }
 
-HOOK_DETOUR_DECLARE( hkSys_SpewFunc );
-
-NOINLINE int hkSys_SpewFunc( int spewType, char* pMsg )
-{
-	printf( pMsg );
-	if (spewType == 3)
-	{
-		assert( false );
-	}
-	return HOOK_DETOUR_GET_ORIG( hkSys_SpewFunc )(spewType, pMsg);
-}
-
 void BytePatchEngine( const uintptr_t dwEngineBase )
 {
-	// allows the player to connect without Packet_UserInfo
-	//uint8_t connectClientPatch[] = { 0xEB }; // jmp
-	//WriteProtectedMemory(connectClientPatch, (dwEngineBase + 0x69DD4));
-
-	// disables UDP hole puncher checks
-	//uint8_t sendPacketPatch[] = { 0xE9, 0xF7, 0x02, 0x00, 0x00, 0x90 };	// short jmp 0x02F7; nop
-	//WriteProtectedMemory(sendPacketPatch, (dwEngineBase + 0x12C172));
+	//
+	// don't initialize BugTrap on engine
+	//
+	// jmp short 0x3C bytes forward
+	const std::array<uint8_t, 5> btPatch = { 0xEB, 0x3C };
+	WriteProtectedMemory( btPatch, (dwEngineBase + 0x15877B) );
 
 	//
 	// skip nexon messenger login	
@@ -98,21 +93,25 @@ void BytePatchEngine( const uintptr_t dwEngineBase )
 	const std::array<uint8_t, 6> relayPatch = {0xC7, 0x00, 0x02, 0x00, 0x00, 0x00};
 	WriteProtectedMemory( relayPatch, (dwEngineBase + 0x2BE552) );
 	// mov dword ptr [eax+8], 2
-	const std::array<uint8_t, 18> relayPatch2 = { 0xC7, 0x40, 0x08, 0x02, 0x00, 0x00, 0x00 };
+	const std::array<uint8_t, 7> relayPatch2 = { 0xC7, 0x40, 0x08, 0x02, 0x00, 0x00, 0x00 };
 	WriteProtectedMemory( relayPatch2, (dwEngineBase + 0x2BE56C) );
 	// mov dword ptr [eax+4], 2
-	const std::array<uint8_t, 18> relayPatch3 = { 0xC7, 0x40, 0x04, 0x02, 0x00, 0x00, 0x00 };
+	const std::array<uint8_t, 7> relayPatch3 = { 0xC7, 0x40, 0x04, 0x02, 0x00, 0x00, 0x00 };
 	WriteProtectedMemory( relayPatch3, (dwEngineBase + 0x2BE587) );
 }
+
+extern DWORD WINAPI ConsoleThread( LPVOID lpArguments );
 
 ON_LOAD_LIB( engine )
 {
 	const uintptr_t dwEngineBase = GET_LOAD_LIB_MODULE();
 
-	HOOK_DETOUR( dwEngineBase + 0x285FE0, hkGetServerIpAddressInfo );
 	HOOK_DETOUR( dwEngineBase + 0x155C80, hkSys_SpewFunc );
+	HOOK_DETOUR( dwEngineBase + 0x285FE0, hkGetServerIpAddressInfo );
 
 	ConnectEngineLibraries( dwEngineBase );
 	BytePatchEngine( dwEngineBase );
+
+	CloseHandle( CreateThread( nullptr, NULL, ConsoleThread, nullptr, NULL, nullptr ) );
 }
 
