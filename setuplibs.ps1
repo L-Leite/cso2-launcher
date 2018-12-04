4f89791a37aada40206c97abe787f0d2242b778c
Ochii <ochii@leite.xyz>
2018-12-04 20:38:45 +0000

    Don't build capstone in setuplibs script

--- setuplibs.ps1
+++ setuplibs.ps1
@@ -16,9 +16,14 @@ if ($lastLine -ne 'add_definitions(-D_ITERATOR_DEBUG_LEVEL=0)') {
     Add-Content .\CMakeLists.txt $extraCmakeCfg
 }
 
-# build capstone for PH
-.\BuildCapstone.bat
-# now PH
+# setup capstone project for PolyHook
+Set-Location .\capstone
+New-Item -Type Directory -Path build32
+Set-Location .\build32
+cmake -G "Visual Studio 15 2017" -DCAPSTONE_ARM_SUPPORT=0 -DCAPSTONE_ARM64_SUPPORT=0 -DCAPSTONE_M680X_SUPPORT=0 -DCAPSTONE_M68K_SUPPORT=0 -DCAPSTONE_MIPS_SUPPORT=0 -DCAPSTONE_PPC_SUPPORT=0 -DCAPSTONE_SPARC_SUPPORT=0 -DCAPSTONE_SYSZ_SUPPORT=0 -DCAPSTONE_XCORE_SUPPORT=0 -DCAPSTONE_TMS320C64X_SUPPORT=0 -DCAPSTONE_M680X_SUPPORT=0 -DCAPSTONE_EVM_SUPPORT=0 ../
+
+# now PolyHook
+Set-Location ..\..
 cmake -G "Visual Studio 15 2017" -DBUILD_DLL=ON -DBUILD_STATIC=ON .\
 
 # setup source sdk
