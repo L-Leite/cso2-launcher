318bf24e9b4d13eee49346e6ce8b17966a73cc3c
Ochii <ochii@leite.xyz>
2019-02-07 21:59:34 +0000

    Allow the user to load decrypted files through '-decryptedfiles' command line argument

--- launcher/source/hooks/filesystem.cpp
+++ launcher/source/hooks/filesystem.cpp
@@ -1,24 +1,39 @@
 #include "stdafx.hpp"
 
 #include "hooks.hpp"
+#include "tier0/ICommandLine.h"
 
 void BytePatchFilesystem( const uintptr_t dwFilesystemBase )
 {
     //
     // IsFileEncrypted (1006A3B0, rva 6A3B0 @ FileSystem_Stdio.dll)
-	// if the requested file's extension contains 'txt', 'csv', 'cfg' or 'db',
-	// then it's an encrypted file ('etxt', 'ecsv', 'ecfg' or 'edb')
-	// and returns true
-	// 
-	// this patch makes the function return false all the time
-	// WARNING: encrypted files no longer work with this
+    // if the requested file's extension contains 'txt', 'csv', 'cfg' or 'db',
+    // then it's an encrypted file ('etxt', 'ecsv', 'ecfg' or 'edb')
+    // and returns true
     //
-    // mov al, 1; retn
-    const std::array<uint8_t, 5> encryptPatch = { 0xB0, 0x00, 0xC3 };
-    WriteProtectedMemory( encryptPatch, ( dwFilesystemBase + 0x6A3B0 ) );
+    // this patch makes the function return false all the time
+    // WARNING: encrypted files no longer work with this
+    //
+    // only do this if the user passes the '-decryptedfiles' cmd argument
+    if ( CommandLine()->CheckParm( "-decryptedfiles", nullptr ) )
+    {
+        // mov al, 1; retn
+        const std::array<uint8_t, 5> encryptPatch = { 0xB0, 0x00, 0xC3 };
+        utils::WriteProtectedMemory( encryptPatch,
+                                     ( dwFilesystemBase + 0x6A3B0 ) );
+    }
 }
 
 void OnFileSystemLoaded( const uintptr_t dwFilesystemBase )
 {
-    BytePatchFilesystem( dwFilesystemBase );
+    static bool bHasLoaded = false;
+
+    if ( bHasLoaded )
+    {
+        return;
+    }
+
+    bHasLoaded = true;
+
+	BytePatchFilesystem( dwFilesystemBase );
 }
