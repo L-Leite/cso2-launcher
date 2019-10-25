56708d82b3ad19a463e005cbdc70e9ad66b8e1c1
Luís Leite <luis@leite.xyz>
2019-10-25 22:07:00 +0100

    Fix MSVC compilation errors with tier0 memory headers

--- sdk/public/tier0/memalloc.h
+++ sdk/public/tier0/memalloc.h
@@ -52,18 +52,18 @@ typedef size_t (*MemAllocFailHandler_t)( size_t );
 abstract_class IMemAlloc
 {
 public:
+	virtual void *AllocDbg( size_t nSize, const char *pFileName, int nLine ) = 0;
 	virtual void *Alloc( size_t nSize ) = 0;
-    virtual void *AllocDbg( size_t nSize, const char *pFileName, int nLine ) = 0;
-
+    
+	virtual void *ReallocDbg( void *pMem, size_t nSize, const char *pFileName, int nLine ) = 0;
 	virtual void *Realloc( void *pMem, size_t nSize ) = 0;
-    virtual void *ReallocDbg( void *pMem, size_t nSize, const char *pFileName, int nLine ) = 0;
-
+    
+	virtual void FreeDbg( void *pMem, const char *pFileName, int nLine ) = 0;
 	virtual void Free( void *pMem ) = 0;
-    virtual void FreeDbg( void *pMem, const char *pFileName, int nLine ) = 0;
-
-    virtual void *Expand_NoLongerSupported( void *pMem, size_t nSize ) = 0;
+    
     virtual void *Expand_NoLongerSupportedDbg( void *pMem, size_t nSize, const char *pFileName, int nLine ) = 0;
-
+	virtual void *Expand_NoLongerSupported( void *pMem, size_t nSize ) = 0;
+    
 	// Returns size of a particular allocation
 	virtual size_t GetSize( void *pMem ) = 0;
 
--- sdk/public/tier0/memdbgon.h
+++ sdk/public/tier0/memdbgon.h
@@ -11,20 +11,21 @@
 // to include this potentially multiple times (since we can deactivate debugging
 // by including memdbgoff.h)
 
-#if !defined(STEAM) && !defined(NO_MALLOC_OVERRIDE)
+#if !defined( STEAM ) && !defined( NO_MALLOC_OVERRIDE )
 
 // SPECIAL NOTE #2: This must be the final include in a .cpp or .h file!!!
 
-#if defined(_DEBUG) && !defined(USE_MEM_DEBUG)
+#if defined( _DEBUG ) && !defined( USE_MEM_DEBUG )
 #define USE_MEM_DEBUG 1
 #endif
 
-#if defined(NO_HOOK_MALLOC)
+#if defined( NO_HOOK_MALLOC )
 #undef USE_MEM_DEBUG
 #endif
 
-// If debug build or ndebug and not already included MS custom alloc files, or already included this file
-#if (defined(_DEBUG) || !defined(_INC_CRTDBG)) || defined(MEMDBGON_H)
+// If debug build or ndebug and not already included MS custom alloc files, or
+// already included this file
+#if ( defined( _DEBUG ) || !defined( _INC_CRTDBG ) ) || defined( MEMDBGON_H )
 
 #include "basetypes.h"
 #ifdef _WIN32
@@ -32,39 +33,40 @@
 #else
 #include <wchar.h>
 #endif
-#include <string.h>
 #include <malloc.h>
+#include <string.h>
 #include "commonmacros.h"
 #include "memalloc.h"
 
-#if defined(USE_MEM_DEBUG)
-	#if defined( POSIX )
-	
-		#define _NORMAL_BLOCK 1
-		
-		#include <cstddef>
-		#include <glob.h>
-		#include <new>
-		#include <sys/types.h>
-		#if !defined( DID_THE_OPERATOR_NEW )
-                        #define DID_THE_OPERATOR_NEW
-			// posix doesn't have a new of this form, so we impl our own
-			void* operator new( size_t nSize, int blah, const char *pFileName, int nLine );
-			void* operator new[]( size_t nSize, int blah, const char *pFileName, int nLine );
-		#endif
-	
-	#else // defined(POSIX)
-	
-		// Include crtdbg.h and make sure _DEBUG is set to 1.
-		#if !defined(_DEBUG)
-			#define _DEBUG 1
-			#include <crtdbg.h>
-			#undef _DEBUG
-		#else
-			#include <crtdbg.h>
-		#endif // !defined(_DEBUG)
-	
-	#endif // defined(POSIX)
+#if defined( USE_MEM_DEBUG )
+#if defined( POSIX )
+
+#define _NORMAL_BLOCK 1
+
+#include <glob.h>
+#include <sys/types.h>
+#include <cstddef>
+#include <new>
+#if !defined( DID_THE_OPERATOR_NEW )
+#define DID_THE_OPERATOR_NEW
+// posix doesn't have a new of this form, so we impl our own
+void* operator new( size_t nSize, int blah, const char* pFileName, int nLine );
+void* operator new[]( size_t nSize, int blah, const char* pFileName,
+                      int nLine );
+#endif
+
+#else  // defined(POSIX)
+
+// Include crtdbg.h and make sure _DEBUG is set to 1.
+#if !defined( _DEBUG )
+#define _DEBUG 1
+#include <crtdbg.h>
+#undef _DEBUG
+#else
+#include <crtdbg.h>
+#endif  // !defined(_DEBUG)
+
+#endif  // defined(POSIX)
 #endif
 
 #include "tier0/memdbgoff.h"
@@ -84,36 +86,38 @@
 #undef _aligned_free
 
 #ifndef MEMDBGON_H
-inline void *MemAlloc_InlineCallocMemset( void *pMem, size_t nCount, size_t nElementSize)
+inline void* MemAlloc_InlineCallocMemset( void* pMem, size_t nCount,
+                                          size_t nElementSize )
 {
-	memset(pMem, 0, nElementSize * nCount);
-	return pMem;
+    memset( pMem, 0, nElementSize * nCount );
+    return pMem;
 }
 #endif
 
-#define calloc(c, s)		MemAlloc_InlineCallocMemset(malloc(c*s), c, s)
-#define free(p)				g_pMemAlloc->Free( p )
-#define _msize(p)			g_pMemAlloc->GetSize( p )
-#define _expand(p, s)		_expand_NoLongerSupported(p, s)
-#define _aligned_free( p )	MemAlloc_FreeAligned( p )
+#define calloc( c, s ) MemAlloc_InlineCallocMemset( malloc( c* s ), c, s )
+#define free( p ) g_pMemAlloc->Free( p )
+#define _msize( p ) g_pMemAlloc->GetSize( p )
+#define _expand( p, s ) _expand_NoLongerSupported( p, s )
+#define _aligned_free( p ) MemAlloc_FreeAligned( p )
 
 // --------------------------------------------------------
 // Debug path
-#if defined(USE_MEM_DEBUG)
+#if defined( USE_MEM_DEBUG )
 
-#define malloc(s)				g_pMemAlloc->Alloc( s, __FILE__, __LINE__)
-#define realloc(p, s)			g_pMemAlloc->Realloc( p, s, __FILE__, __LINE__ )
-#define _aligned_malloc( s, a )	MemAlloc_AllocAligned( s, a, __FILE__, __LINE__ )
+#define malloc( s ) g_pMemAlloc->AllocDbg( s, __FILE__, __LINE__ )
+#define realloc( p, s ) g_pMemAlloc->ReallocDbg( p, s, __FILE__, __LINE__ )
+#define _aligned_malloc( s, a ) \
+    MemAlloc_AllocAligned( s, a, __FILE__, __LINE__ )
 
-#define _malloc_dbg(s, t, f, l)	WHYCALLINGTHISDIRECTLY(s)
+#define _malloc_dbg( s, t, f, l ) WHYCALLINGTHISDIRECTLY( s )
 
 #if !defined( LINUX )
-#if defined(__AFX_H__) && defined(DEBUG_NEW)
-	#define new DEBUG_NEW
+#if defined( __AFX_H__ ) && defined( DEBUG_NEW )
+#define new DEBUG_NEW
 #else
-	#undef new
-	#define MEMALL_DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
-	#define new MEMALL_DEBUG_NEW
+#undef new
+#define MEMALL_DEBUG_NEW new ( _NORMAL_BLOCK, __FILE__, __LINE__ )
+#define new MEMALL_DEBUG_NEW
 #endif
 #endif
 
@@ -122,58 +126,62 @@ inline void *MemAlloc_InlineCallocMemset( void *pMem, size_t nCount, size_t nEle
 #undef _wcsdup
 #undef wcsdup
 
-#define _strdup(s) MemAlloc_StrDup(s, __FILE__, __LINE__)
-#define strdup(s)  MemAlloc_StrDup(s, __FILE__, __LINE__)
-#define _wcsdup(s) MemAlloc_WcStrDup(s, __FILE__, __LINE__)
-#define wcsdup(s)  MemAlloc_WcStrDup(s, __FILE__, __LINE__)
+#define _strdup( s ) MemAlloc_StrDup( s, __FILE__, __LINE__ )
+#define strdup( s ) MemAlloc_StrDup( s, __FILE__, __LINE__ )
+#define _wcsdup( s ) MemAlloc_WcStrDup( s, __FILE__, __LINE__ )
+#define wcsdup( s ) MemAlloc_WcStrDup( s, __FILE__, __LINE__ )
 
 // Make sure we don't define strdup twice
-#if !defined(MEMDBGON_H)
+#if !defined( MEMDBGON_H )
 
-inline char *MemAlloc_StrDup(const char *pString, const char *pFileName, unsigned nLine)
+inline char* MemAlloc_StrDup( const char* pString, const char* pFileName,
+                              unsigned nLine )
 {
-	char *pMemory;
-	
-	if (!pString)
-		return NULL;
-	
-	size_t len = strlen(pString) + 1;
-	if ((pMemory = (char *)g_pMemAlloc->Alloc(len, pFileName, nLine)) != NULL)
-	{
-		return strcpy( pMemory, pString );
-	}
-	
-	return NULL;
+    char* pMemory;
+
+    if ( !pString )
+        return NULL;
+
+    size_t len = strlen( pString ) + 1;
+    if ( ( pMemory = (char*)g_pMemAlloc->AllocDbg( len, pFileName, nLine ) ) !=
+         NULL )
+    {
+        return strcpy( pMemory, pString );
+    }
+
+    return NULL;
 }
 
-inline wchar_t *MemAlloc_WcStrDup(const wchar_t *pString, const char *pFileName, unsigned nLine)
+inline wchar_t* MemAlloc_WcStrDup( const wchar_t* pString,
+                                   const char* pFileName, unsigned nLine )
 {
-	wchar_t *pMemory;
-	
-	if (!pString)
-		return NULL;
-	
-	size_t len = (wcslen(pString) + 1);
-	if ((pMemory = (wchar_t *)g_pMemAlloc->Alloc(len * sizeof(wchar_t), pFileName, nLine)) != NULL)
-	{
-		return wcscpy( pMemory, pString );
-	}
-	
-	return NULL;
+    wchar_t* pMemory;
+
+    if ( !pString )
+        return NULL;
+
+    size_t len = ( wcslen( pString ) + 1 );
+    if ( ( pMemory = (wchar_t*)g_pMemAlloc->AllocDbg(
+               len * sizeof( wchar_t ), pFileName, nLine ) ) != NULL )
+    {
+        return wcscpy( pMemory, pString );
+    }
+
+    return NULL;
 }
 
-#endif // DBMEM_DEFINED_STRDUP
+#endif  // DBMEM_DEFINED_STRDUP
 
 #else
 // --------------------------------------------------------
 // Release path
 
-#define malloc(s)				g_pMemAlloc->Alloc( s )
-#define realloc(p, s)			g_pMemAlloc->Realloc( p, s )
-#define _aligned_malloc( s, a )	MemAlloc_AllocAligned( s, a )
+#define malloc( s ) g_pMemAlloc->Alloc( s )
+#define realloc( p, s ) g_pMemAlloc->Realloc( p, s )
+#define _aligned_malloc( s, a ) MemAlloc_AllocAligned( s, a )
 
 #ifndef _malloc_dbg
-#define _malloc_dbg(s, t, f, l)	WHYCALLINGTHISDIRECTLY(s)
+#define _malloc_dbg( s, t, f, l ) WHYCALLINGTHISDIRECTLY( s )
 #endif
 
 #undef new
@@ -183,66 +191,69 @@ inline wchar_t *MemAlloc_WcStrDup(const wchar_t *pString, const char *pFileName,
 #undef _wcsdup
 #undef wcsdup
 
-#define _strdup(s) MemAlloc_StrDup(s)
-#define strdup(s)  MemAlloc_StrDup(s)
-#define _wcsdup(s) MemAlloc_WcStrDup(s)
-#define wcsdup(s)  MemAlloc_WcStrDup(s)
+#define _strdup( s ) MemAlloc_StrDup( s )
+#define strdup( s ) MemAlloc_StrDup( s )
+#define _wcsdup( s ) MemAlloc_WcStrDup( s )
+#define wcsdup( s ) MemAlloc_WcStrDup( s )
 
 // Make sure we don't define strdup twice
-#if !defined(MEMDBGON_H)
+#if !defined( MEMDBGON_H )
 
-inline char *MemAlloc_StrDup(const char *pString)
+inline char* MemAlloc_StrDup( const char* pString )
 {
-	char *pMemory;
+    char* pMemory;
 
-	if (!pString)
-		return NULL;
+    if ( !pString )
+        return NULL;
 
-	size_t len = strlen(pString) + 1;
-	if ((pMemory = (char *)g_pMemAlloc->Alloc(len)) != NULL)
-	{
-		return strcpy( pMemory, pString );
-	}
+    size_t len = strlen( pString ) + 1;
+    if ( ( pMemory = (char*)g_pMemAlloc->Alloc( len ) ) != NULL )
+    {
+        return strcpy( pMemory, pString );
+    }
 
-	return NULL;
+    return NULL;
 }
 
-inline wchar_t *MemAlloc_WcStrDup(const wchar_t *pString)
+inline wchar_t* MemAlloc_WcStrDup( const wchar_t* pString )
 {
-	wchar_t *pMemory;
+    wchar_t* pMemory;
 
-	if (!pString)
-		return NULL;
+    if ( !pString )
+        return NULL;
 
-	size_t len = (wcslen(pString) + 1);
-	if ((pMemory = (wchar_t *)g_pMemAlloc->Alloc(len * sizeof(wchar_t))) != NULL)
-	{
-		return wcscpy( pMemory, pString );
-	}
+    size_t len = ( wcslen( pString ) + 1 );
+    if ( ( pMemory = (wchar_t*)g_pMemAlloc->Alloc(
+               len * sizeof( wchar_t ) ) ) != NULL )
+    {
+        return wcscpy( pMemory, pString );
+    }
 
-	return NULL;
+    return NULL;
 }
 
-#endif // DBMEM_DEFINED_STRDUP
+#endif  // DBMEM_DEFINED_STRDUP
 
-#endif // USE_MEM_DEBUG
+#endif  // USE_MEM_DEBUG
 
-#define MEMDBGON_H // Defined here so can be used above
+#define MEMDBGON_H  // Defined here so can be used above
 
 #else
 
-#if defined(USE_MEM_DEBUG)
+#if defined( USE_MEM_DEBUG )
 #ifndef _STATIC_LINKED
-#pragma message ("Note: file includes crtdbg.h directly, therefore will cannot use memdbgon.h in non-debug build")
+#pragma message( \
+    "Note: file includes crtdbg.h directly, therefore will cannot use memdbgon.h in non-debug build" )
 #else
-#error "Error: file includes crtdbg.h directly, therefore will cannot use memdbgon.h in non-debug build. Not recoverable in static build"
+#error \
+    "Error: file includes crtdbg.h directly, therefore will cannot use memdbgon.h in non-debug build. Not recoverable in static build"
 #endif
 #endif
-#endif // _INC_CRTDBG
+#endif  // _INC_CRTDBG
 
 #else
 
 // Needed for MEM_ALLOC_CREDIT(), MemAlloc_Alloc(), etc.
 #include "memalloc.h"
 
-#endif // !STEAM && !NO_MALLOC_OVERRIDE
+#endif  // !STEAM && !NO_MALLOC_OVERRIDE
